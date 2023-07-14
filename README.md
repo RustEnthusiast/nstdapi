5ece57f211f9d73e343d1d5f9c462ac0be682c87
Rust Enthusiast <RustEnthusiast@gmail.com>
2023-07-14 08:44:28 -0500

    Prepare `nstdapi` 0.2.0.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # `nstdapi`
-A helper crate for `nstd` providing manipulation of an item's ABI attributes.
+A helper crate for `nstd` providing manipulation of an item.
+
+# Features
+## `capi`
+- Mark function definitions as `#[no_mangle]`.
+- Set function ABIs to `extern "C"`.
+- Mark static item definitions as `#[no_mangle]`.
+- Set static item declaration ABIs to `extern "C"`.
+- Mark structures, enumerations, and unions as `#[repr(C)]`.
+## `link`
+- Implicitly enables `capi`.
+- Turn function definitions into function declarations.
+- Turn static item definitions into static item declarations.
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,95 @@
-//! A helper crate for `nstd` providing manipulation of an item's ABI attributes.
+//! A helper crate for `nstd` providing manipulation of an item.
 #![no_std]
+use cfg_if::cfg_if;
 use proc_macro::TokenStream;
 
-/// Provides an item with the appropriate attributes and ABI based on the crate's features.
+/// Manipulates an item based on the crate's features.
 #[proc_macro_attribute]
 pub fn nstdapi(_: TokenStream, item: TokenStream) -> TokenStream {
-    #[cfg(not(feature = "capi"))]
-    return item;
-    #[cfg(feature = "capi")]
-    {
-        use quote::ToTokens;
-        use syn::{parse_quote, Item};
-        let mut input = syn::parse(item).expect("failed to parse an item with `nstdapi`");
-        match &mut input {
-            Item::Fn(f) => {
-                f.attrs.push(parse_quote!(#[no_mangle]));
-                f.sig.abi = parse_quote!(extern "C");
+    cfg_if! {
+        if #[cfg(any(feature = "capi", feature = "link"))] {
+            use quote::ToTokens;
+            use syn::{
+                parse_quote, FnArg, ForeignItem, ForeignItemFn, ForeignItemStatic, Item,
+                ItemForeignMod, Pat,
+            };
+            let mut input = syn::parse(item).expect("failed to parse an item with `nstdapi`");
+            if cfg!(feature = "capi") {
+                match &mut input {
+                    Item::Struct(s) => s.attrs.push(parse_quote!(#[repr(C)])),
+                    Item::Enum(e) => e.attrs.push(parse_quote!(#[repr(C)])),
+                    Item::Union(u) => u.attrs.push(parse_quote!(#[repr(C)])),
+                    _ => {}
+                }
             }
-            Item::Static(s) => s.attrs.push(parse_quote!(#[no_mangle])),
-            Item::Struct(s) => s.attrs.push(parse_quote!(#[repr(C)])),
-            Item::Enum(e) => e.attrs.push(parse_quote!(#[repr(C)])),
-            Item::Union(u) => u.attrs.push(parse_quote!(#[repr(C)])),
-            _ => {}
+            if cfg!(feature = "link") {
+                input = match input {
+                    Item::Fn(f) => {
+                        let mut sig = f.sig;
+                        sig.unsafety = None;
+                        sig.constness = None;
+                        for arg in &mut sig.inputs {
+                            match arg {
+                                FnArg::Receiver(arg) => {
+                                    arg.mutability = None;
+                                }
+                                FnArg::Typed(pat) => {
+                                    if let Pat::Ident(arg) = &mut *pat.pat {
+                                        arg.mutability = None;
+                                    }
+                                }
+                            }
+                        }
+                        Item::ForeignMod(ItemForeignMod {
+                            attrs: [parse_quote!(#[allow(unused_attributes)])].into(),
+                            unsafety: None,
+                            #[cfg(not(feature = "capi"))]
+                            abi: parse_quote!(extern "Rust"),
+                            #[cfg(feature = "capi")]
+                            abi: parse_quote!(extern "C"),
+                            brace_token: Default::default(),
+                            items: [ForeignItem::Fn(ForeignItemFn {
+                                attrs: f.attrs,
+                                vis: f.vis,
+                                sig,
+                                semi_token: Default::default(),
+                            })].into(),
+                        })
+                    }
+                    Item::Static(s) => Item::ForeignMod(ItemForeignMod {
+                        attrs: [parse_quote!(#[allow(unused_attributes)])].into(),
+                        unsafety: None,
+                        #[cfg(not(feature = "capi"))]
+                        abi: parse_quote!(extern "Rust"),
+                        #[cfg(feature = "capi")]
+                        abi: parse_quote!(extern "C"),
+                        brace_token: Default::default(),
+                        items: [ForeignItem::Static(ForeignItemStatic {
+                            attrs: s.attrs,
+                            vis: s.vis,
+                            static_token: Default::default(),
+                            mutability: s.mutability,
+                            ident: s.ident,
+                            colon_token: Default::default(),
+                            ty: s.ty,
+                            semi_token: Default::default(),
+                        })].into(),
+                    }),
+                    input => input
+                };
+            } else if cfg!(feature = "capi") {
+                match &mut input {
+                    Item::Fn(f) => {
+                        f.attrs.push(parse_quote!(#[no_mangle]));
+                        f.sig.abi = parse_quote!(extern "C");
+                    }
+                    Item::Static(s) => s.attrs.push(parse_quote!(#[no_mangle])),
+                    _ => {}
+                }
+            }
+            input.into_token_stream().into()
+        } else {
+            item
         }
-        input.into_token_stream().into()
     }
 }
